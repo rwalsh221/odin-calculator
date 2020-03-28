aa4295a7bd5a64a4a1c56922dd7f7ac35ee3b276
Richard Walsh <richwalsh2@googlemail.com>
2020-03-28 17:26:31 +0000

    added working decimal point and zero btn. created operator function. removed individual operator functions.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let keyPress, calcInputDisplay, calcAdd, calcSub, calcMultiply, calcDiv, calcEqual, calcClear, calcInput, calcInputSub, prevKeyArr;
+
+//  VARIABLES
+let calcInputDisplay, calcInputDisplayZero, calcOperator,
+    calcEqual, calcClear, 
+     calcInput, calcInputSub, prevKeyArr, 
+     decimalPoint, calcDPoint;
+
+decimalPoint = false;
 
 prevKeyArr = [];
 
@@ -6,8 +13,8 @@ calcInput = document.querySelector('.calc-screen');
 
 calcInputSub = document.querySelector('.calc-sub');
 
-// calcInput.textContent = 0;
 
+// BUTTON FUNCTION DISPLAYS PRESSED BUTTON
 calcInputDisplay = function(x) {
     
     if (calcInput.textContent === '0') {
@@ -21,6 +28,183 @@ calcInputDisplay = function(x) {
     }
 };
 
+// ZERO BUTTON FUNCTION STOPS ZERO DISPLAYING BEFORE NUMBER 
+calcInputDisplayZero = function() {
+    if (calcInput.textContent != '0' && calcInput.textContent.length <= 9)
+        calcInput.textContent += 0;
+};
+
+// DECIMAL POINT FUNCTION
+calcDPoint = function() {
+    if (calcInput.textContent === '0' && decimalPoint === false) {
+        calcInput.textContent = '0.'
+    } else if (calcInput.textContent.length < 9 && decimalPoint === false) {
+        calcInput.textContent += '.'
+    }
+
+    decimalPoint = true
+    return decimalPoint
+};
+
+// CALCULATOR OPERATOR FUNCTION REPLACE X WITH OPERATOR
+calcOperator = function(x) {
+    let operator = x
+    
+    
+    if (calcInputSub.textContent === '0') {
+        calcInputSub.textContent = calcInput.textContent;
+        calcInput.textContent = '0';
+        
+
+    }   else if (calcInput.textContent != '0' && operator === '+') {
+    
+        let resCalc = (+calcInputSub.textContent) + (+calcInput.textContent);
+        resCalcString = resCalc.toString()
+        calcInputSub.textContent = resCalcString.substring(0,9);
+
+    }   else if (calcInput.textContent != '0' && operator === '-') {
+    
+        let resCalc = (+calcInputSub.textContent) - (+calcInput.textContent);
+        resCalcString = resCalc.toString()
+        calcInputSub.textContent = resCalcString.substring(0,9);
+
+    }   else if (calcInput.textContent != '0' && operator === '*') {
+    
+        let resCalc = (+calcInputSub.textContent) * (+calcInput.textContent);
+        resCalcString = resCalc.toString()
+        calcInputSub.textContent = resCalcString.substring(0,9);
+        calcInput.textContent= '0';
+
+    }   else if (calcInput.textContent != '0' && operator === '/') {
+    
+        let resCalc = (+calcInputSub.textContent) / (+calcInput.textContent);
+        resCalcString = resCalc.toString()
+        calcInputSub.textContent = resCalcString.substring(0,9);
+}   
+    
+    prevKeyArr.push(`${x}`);
+    decimalPoint = false
+    return decimalPoint
+};
+
+// EQUAL FUNCTION
+calcEqual = function() {
+    if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '+' && prevKeyArr[prevKeyArr.length - 1] != '=' ){
+        calcOperator('+');
+        // let sub = calcInputSub.textContent
+        calcInput.textContent = calcInputSub.textContent;
+        calcInputSub.textContent = '0';
+}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '-' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
+        calcOperator('-');
+        // let sub = calcInputSub.textContent
+        calcInput.textContent = calcInputSub.textContent;
+        calcInputSub.textContent = '0';
+}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '*' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
+        calcOperator('*');
+        // let sub = calcInputSub.textContent
+        calcInput.textContent = calcInputSub.textContent;
+        calcInputSub.textContent = '0';
+}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '/' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
+    calcOperator('/');
+    // let sub = calcInputSub.textContent
+    calcInput.textContent = calcInputSub.textContent;
+    calcInputSub.textContent = '0';
+}
+    prevKeyArr.push('=');
+};
+
+// CLEAR DISPLAY
+calcClear = function() {
+    calcInput.textContent = '0';
+    calcInputSub.textContent = '0';
+    decimalPoint = false;
+    return decimalPoint;
+};
+
+
+
+
+// NUMBER KEY INPUT
+
+document.getElementById('key-1').addEventListener('click', function() {
+    calcInputDisplay(1);
+});
+
+document.getElementById('key-2').addEventListener('click', function() {
+    calcInputDisplay(2);
+});
+
+document.getElementById('key-3').addEventListener('click', function() {
+    calcInputDisplay(3);
+});
+
+document.getElementById('key-4').addEventListener('click', function() {
+    calcInputDisplay(4);
+});
+
+document.getElementById('key-5').addEventListener('click', function() {
+    calcInputDisplay(5);
+});
+
+document.getElementById('key-6').addEventListener('click', function() {
+    calcInputDisplay(6);
+});
+
+document.getElementById('key-7').addEventListener('click', function() {
+    calcInputDisplay(7);
+});
+
+document.getElementById('key-8').addEventListener('click', function() {
+    calcInputDisplay(8);
+});
+
+document.getElementById('key-9').addEventListener('click', function() {
+    calcInputDisplay(9);
+});
+
+document.getElementById('key-0').addEventListener('click', function() {
+    calcInputDisplayZero();
+    
+});
+
+// CALC KEY INPUT 
+
+document.getElementById('key-add').addEventListener('click', function() {
+    calcOperator('+');
+    console.log('btnpress-add')
+});
+
+document.getElementById('key-sub').addEventListener('click', function() {
+    calcOperator('-');
+    console.log('btnpress-minus')
+});
+
+document.getElementById('key-mul').addEventListener('click', function() {
+    calcOperator('*');
+    console.log('btnpress-multiply')
+});
+
+document.getElementById('key-divide').addEventListener('click', function() {
+    calcOperator('/');
+    console.log('btnpress-div')
+});
+
+document.getElementById('key-equal').addEventListener('click', function() {
+    calcEqual();
+});
+
+document.getElementById('key-clear').addEventListener('click', function() {
+    calcClear();
+});
+
+document.getElementById('key-dpoint').addEventListener('click', function() {
+    calcDPoint();
+});
+
+
+
+/// FIRST TRY AND NOTES**********************************************************************************************************************************
+/*
 calcAdd = function() {
     if (calcInputSub.textContent === '0') {
         calcInputSub.textContent = calcInput.textContent;
@@ -28,14 +212,15 @@ calcAdd = function() {
       
         
     } else if (calcInput.textContent != '0') {
-        let addCalc = (+calcInputSub.textContent) + (+calcInput.textContent);
-        calcInputSub.textContent = addCalc;
+        // let addCalc = (+calcInputSub.textContent) + (+calcInput.textContent);
+        calcInputSub.textContent = (+calcInputSub.textContent) + (+calcInput.textContent);
         calcInput.textContent= '0';
-        calcDisplay = '0';
+        // calcDisplay = '0';
     }
 
     prevKeyArr.push('+');
-    
+    decimalPoint = false
+    return decimalPoint
 }
 
 calcSub = function() {
@@ -85,105 +270,5 @@ calcDiv = function() {
     }
 
     prevKeyArr.push('/');
-}
-
-calcEqual = function() {
-    if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '+' && prevKeyArr[prevKeyArr.length - 1] != '=' ){
-        calcAdd();
-        // let sub = calcInputSub.textContent
-        calcInput.textContent = calcInputSub.textContent;
-        calcInputSub.textContent = '0';
-}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '-' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
-        calcSub();
-        // let sub = calcInputSub.textContent
-        calcInput.textContent = calcInputSub.textContent;
-        calcInputSub.textContent = '0';
-}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '*' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
-        calcMultiply();
-        // let sub = calcInputSub.textContent
-        calcInput.textContent = calcInputSub.textContent;
-        calcInputSub.textContent = '0';
-}   else if (calcInputSub != '0' && prevKeyArr[prevKeyArr.length - 1] === '/' && prevKeyArr[prevKeyArr.length - 1] != '=' ) {
-    calcDiv();
-    // let sub = calcInputSub.textContent
-    calcInput.textContent = calcInputSub.textContent;
-    calcInputSub.textContent = '0';
-}
-    prevKeyArr.push('=');
 };
-
-calcClear = function() {
-    calcInput.textContent = '0';
-    calcInputSub.textContent = '0';
-};
-
-
-
-
-// NUMBER KEY INPUT
-
-document.getElementById('key-1').addEventListener('click', function() {
-    calcInputDisplay(1);
-});
-
-document.getElementById('key-2').addEventListener('click', function() {
-    calcInputDisplay(2);
-});
-
-document.getElementById('key-3').addEventListener('click', function() {
-    calcInputDisplay(3);
-});
-
-document.getElementById('key-4').addEventListener('click', function() {
-    calcInputDisplay(4);
-});
-
-document.getElementById('key-5').addEventListener('click', function() {
-    calcInputDisplay(5);
-});
-
-document.getElementById('key-6').addEventListener('click', function() {
-    calcInputDisplay(6);
-});
-
-document.getElementById('key-7').addEventListener('click', function() {
-    calcInputDisplay(7);
-});
-
-document.getElementById('key-8').addEventListener('click', function() {
-    calcInputDisplay(8);
-});
-
-document.getElementById('key-9').addEventListener('click', function() {
-    calcInputDisplay(9);
-});
-
-// CALC KEY INPUT 
-
-document.getElementById('key-add').addEventListener('click', function() {
-    calcAdd();
-    console.log('btnpress-add')
-});
-
-document.getElementById('key-sub').addEventListener('click', function() {
-    calcSub();
-    console.log('btnpress-minus')
-});
-
-document.getElementById('key-mul').addEventListener('click', function() {
-    calcMultiply();
-    console.log('btnpress-multiply')
-});
-
-document.getElementById('key-divide').addEventListener('click', function() {
-    calcDiv();
-    console.log('btnpress-div')
-});
-
-document.getElementById('key-equal').addEventListener('click', function() {
-    calcEqual();
-});
-
-document.getElementById('key-clear').addEventListener('click', function() {
-    calcClear();
-});
+*/
