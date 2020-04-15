ffe687feb17aaad18998041cb55714f1a3673e77
Richard Walsh <richwalsh2@googlemail.com>
2020-04-15 17:20:04 +0100

    added keypress function to all keys. added btn-active to animate key on keypress. testing switch statement

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 let calcInputDisplay, calcInputDisplayZero, calcOperator,
     calcEqual, calcClear, 
      calcInput, calcInputSub, prevKeyArr, 
-     decimalPoint, calcDPoint;
+     decimalPoint, calcDPoint,
+     addActiveClass, removeActiveClass;
+     
 
 decimalPoint = false;
 
@@ -12,6 +14,7 @@ prevKeyArr = [];
 calcInput = document.querySelector('.calc-screen');
 
 calcInputSub = document.querySelector('.calc-sub');
+
 
 
 // BUTTON FUNCTION DISPLAYS PRESSED BUTTON
@@ -126,8 +129,256 @@ calcClear = function() {
     return decimalPoint;
 };
 
+// KEYPRESS FUNCTION ADD ACTIVE CLASS
 
+document.body.addEventListener('keydown', function(event) {
+    let char = event.char || event.charCode|| event.which
+    let keyInput = event.key
 
+    switch(char) {
+        // 1 KEY
+        case 49:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 97:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 2 key
+        case 50:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 98:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 3 KEY
+        case 51:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 99:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 4 KEY
+        case 52:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 100:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 5 KEY
+        case 53:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 101:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 6 KEY
+        case 54:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 102:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 7 KEY
+        case 55:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 103:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 8 KEY
+        case 56:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 104:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 9 KEY
+        case 57:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 105:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        // 0 key
+        case 48: 
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+        case 96:
+            calcInputDisplay(keyInput);
+            document.getElementById(`key-${keyInput}`).classList.add('btn-active');
+        break;
+
+        // + key
+        case 107:
+            calcOperator(keyInput);
+            document.getElementById(`key-add`).classList.add('btn-active');
+        break;
+        // - key
+        case 109:
+            calcOperator(keyInput);
+            document.getElementById(`key-sub`).classList.add('btn-active');
+        break;
+        // * key
+        case 106:
+            calcOperator(keyInput);
+            document.getElementById(`key-mul`).classList.add('btn-active');
+        break;
+        // / key
+        case 111:
+            calcOperator(keyInput);
+            document.getElementById(`key-divide`).classList.add('btn-active');
+        break;
+
+        // = key
+        case 13:
+            calcEqual();
+            document.getElementById(`key-equal`).classList.add('btn-active');
+        break;
+        case 187:
+            calcEqual();
+            document.getElementById(`key-equal`).classList.add('btn-active');
+
+        // . key
+        case 110:
+            calcDPoint();
+            document.getElementById(`key-dpoint`).classList.add('btn-active');
+        break;
+        case 190:
+            calcDPoint();
+            document.getElementById(`key-dpoint`).classList.add('btn-active');
+        break;
+    }
+});
+
+// KEYPRESS FUNCTION REMOVE ACTIVE CLASS
+
+document.body.addEventListener('keyup', function(event) {
+    let char = event.char || event.charCode|| event.which;
+    let keyInput = event.key
+    switch(char) {
+        // 1 KEY
+        case 49:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 97:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 2 key
+        case 50:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 98:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 3 KEY
+        case 51:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 99:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 4 KEY
+        case 52:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 100:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 5 KEY
+        case 53:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 101:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 6 KEY
+        case 54:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 102:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 7 KEY
+        case 55:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 103:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 8 KEY
+        case 56:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 104:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 9 KEY
+        case 57:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 105:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        // 0 key
+        case 48: 
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+        case 96:
+            document.getElementById(`key-${keyInput}`).classList.remove('btn-active');
+        break;
+
+        // + key
+        case 107:
+            document.getElementById(`key-add`).classList.remove('btn-active');
+        break;
+        // - key
+        case 109:
+            document.getElementById(`key-sub`).classList.remove('btn-active');
+        break;
+        // * key
+        case 106:
+            document.getElementById(`key-mul`).classList.remove('btn-active');
+        break;
+        // / key
+        case 111:
+            document.getElementById(`key-divide`).classList.remove('btn-active');
+        break;
+
+        // = key
+        case 13:
+            document.getElementById(`key-equal`).classList.remove('btn-active');
+        break;
+        case 187:
+            document.getElementById(`key-equal`).classList.remove('btn-active');
+
+        // . key
+        case 110:
+            document.getElementById(`key-dpoint`).classList.remove('btn-active');
+        break;
+        case 190:
+            document.getElementById(`key-dpoint`).classList.remove('btn-active');
+        break;
+    }
+});
 
 // NUMBER KEY INPUT
 
@@ -172,7 +423,7 @@ document.getElementById('key-0').addEventListener('click', function() {
     
 });
 
-// CALC KEY INPUT 
+// CALC OPERATOR INPUT 
 
 document.getElementById('key-add').addEventListener('click', function() {
     calcOperator('+');
